b3a3e10620e1b836a10aa60496814bd2ae37e452
DaehyeonKim <Daehyeonoff@gmail.com>
2022-08-14 19:39:13 +0900

    20220814

--- src/component/Join.js
+++ src/component/Join.js
@@ -10,14 +10,14 @@ function Join() {
             <div className="background22">
                 <div className="modal22">
                     <form>
-                        <span className="pinkSpan22">계정 만들기</span>
+                        <span className="pinkSpan22">계정 생성</span>
                         <p className="id22">아이디</p>
                         <input className="id221"></input>
                         <p className="pw22">비밀번호</p>
                         <input className="pw221"></input>
                         <p className="nick22">닉네임</p>
                         <input className="nick221"></input>
-                        <button className="make22">만들기</button>
+                        <button className="make22">생성</button>
                     </form>
                 </div>
             </div>
